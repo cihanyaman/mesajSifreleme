73e1a71502c0f0f2c7b2b05e87b04082f1a5e65a
Cihan Yaman <76009956+cihanyaman@users.noreply.github.com>
2021-12-31 21:32:58 +0300

    mesaj şifreleme

---
+++ mesaj şifreleme/Encryption/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Form_Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip explanation = new ToolTip();
+            explanation.ToolTipTitle = "Instruction!";
+            explanation.ToolTipIcon = ToolTipIcon.Warning;
+            explanation.IsBalloon = true;
+
+            explanation.SetToolTip(btn_Decrypt, "Düğmeye tıklamadan önce şifresi çözülen metni üstteki metin kutusuna yapıştırın");
+        }
+        #endregion
+
+
+        #region ChecBoxes
+        private void checkBoxSPN_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxSPN.Checked == true)
+            {
+                checkBoxSHA.Checked = false;
+            }
+            
+        }
+
+        private void checkBoxSHA_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxSHA.Checked == true)
+            {
+                checkBoxSPN.Checked = false;
+            }
+        }
+        #endregion
+
+
+        #region Encrytption Buttons
+        private void btn_Encrypt_Click(object sender, EventArgs e)
+        {
+            txtPlain.Text = non_T_Chars(txtPlain.Text);
+            if (txtPlain.Text == "") { MessageBox.Show("metin boş olamaz.", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }   // boş olamaz
+            else if(txtKey.Text == "") { MessageBox.Show("anahtar boş olamaz", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+            else if (txtKey.Text.Length != 8) { MessageBox.Show("Anahtar 8 karakter uzunluğunda olmalıdır.", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+            else
+            {
+                labelPText.Text = "Plain Text";
+                labelCipher.Text = "Encrypted Text";
+                if (checkBoxSPN.Checked == true )
+                {
+                    if (txtPlain.Text.Length % 2 == 1) { txtPlain.Text += " "; } // Spn her adımda 2 karakterle çalışır, bu nedenle Metin çift karakter uzunluğunda olmalıdır.
+                    else
+                    {
+                        SPN_encryption spn = new SPN_encryption(txtPlain.Text, txtKey.Text);
+                        txtCipher.Text = spn.encrypt();
+                    }
+                }
+                else if (checkBoxSHA.Checked == true )
+                {
+                    SHA_encryption sha = new SHA_encryption(txtPlain.Text);
+                    txtCipher.Text = sha.encrypt();
+                }
+                else if (checkBoxSPN.Checked == false && checkBoxSHA.Checked == false) { MessageBox.Show("İstediğiniz şifreleme yöntemini seçin ", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+
+            }
+        }
+
+        private void btn_Decrypt_Click(object sender, EventArgs e)
+        {
+            if (txtPlain.Text == "") { MessageBox.Show("boş olamaz", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+            else if (txtKey.Text == "") { MessageBox.Show("anahtar boş olamaz.", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+            else if (txtKey.Text.Length != 8) { MessageBox.Show("anahtar 8 karekterli olmalı.", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+            else
+            {
+                labelPText.Text = "Encrypted Text";
+                labelCipher.Text = "Decrypted Text";
+
+                if (checkBoxSPN.Checked == true)
+                {
+                    SPN_encryption spn = new SPN_encryption(txtKey.Text);
+                    txtCipher.Text = spn.decrypt(txtPlain.Text);
+                }
+                else if (checkBoxSHA.Checked == true) { MessageBox.Show("SHA ya bağlanılamadı "); }
+                else if (checkBoxSPN.Checked == false && checkBoxSHA.Checked == false) { MessageBox.Show("İstediğiniz şifre çözme yöntemini seçin ", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+            }
+        }
+        #endregion
+
+
+        #region Turkish characters Replace
+        private string non_T_Chars(string text)
+        {
+            text = text.Replace("İ", "I");
+            text = text.Replace("ı", "i");
+            text = text.Replace("Ğ", "G");
+            text = text.Replace("ğ", "g");
+            text = text.Replace("Ö", "O");
+            text = text.Replace("ö", "o");
+            text = text.Replace("Ü", "U");
+            text = text.Replace("ü", "u");
+            text = text.Replace("Ş", "S");
+            text = text.Replace("ş", "s");
+            text = text.Replace("Ç", "C");
+            text = text.Replace("ç", "c");
+            return text;
+        }
+        #endregion
+
+    }
+}
---
+++ mesaj şifreleme/Encryption/nunitTest.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPN_16_Sifreleme
+{
+    [TestFixture] // bu ifade bize bu sınıfın bir test sınıfı olduğunu ifade etmektedir
+    class nunitTest
+    {
+        nunitTest eD = new nunitTest(); // burada EncryptionDecryption sınıfımızdan bir nesne oluşturduk
+
+        [Test] //bu ifade bize bu metodun test metodu olduğunu ifade etmektedir
+        public void StringToBinary() //StringToBinary test metodumuz
+        {
+            string sonuc = eD.StringToBinary("na"); //StringToBinary methoduna değerimizi parametre olarak geçirdik, ve EncryptionDecryption sınıfının metodunu çağırdık
+            Assert.AreEqual("0110111001100001", sonuc); //ilk parametre 2. parametre ile aynı ise methodumuz doğru çalışıyordur ve test başarılıdır
+        }
+
+        private string StringToBinary(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test] //bu ifade bize bu metodun test metodu olduğunu ifade etmektedir
+        //
+        public void BinaryToString() //BinaryToString test metodumuz
+        {
+            string sonuc = eD.BinaryToString("0110111001100001"); //BinaryToString methoduna değerimizi parametre olarak geçirdik, ve EncryptionDecryption sınıfının metodunu çağırdık
+            Assert.AreEqual("na", sonuc); //ilk parametre 2. parametre ile aynı ise methodumuz doğru çalışıyordur ve test başarılıdır
+        }
+
+        private string BinaryToString(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test] //bu ifade bize bu metodun test metodu olduğunu ifade etmektedir
+        public void SPN_encryption() //metin test metodumuz
+        {
+            string sonuc = eD.SPN_encryption("adana"); //metin methoduna değerimizi parametre olarak geçirdik, ve EncryptionDecryption sınıfının metodunu çağırdık
+            Assert.AreEqual("101011100110001100101110011100111010111101000011", sonuc); //ilk parametre(mutluyum kelimesinin spn16 ile şifrelenmiş binary değeri) 2. parametre ile aynı ise şifreleme methodumuz doğru çalışıyordur ve test başarılıdır
+        }
+
+        private string SPN_encryption(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test] //bu ifade bize bu metodun test metodu olduğunu ifade etmektedir
+        public void metinCoz() //metinCoz test metodumuz
+        {
+            string sonuc = eD.metinCoz("1110111001101101000011000110101101001110010011110000111001101111"); //metinCoz methoduna değerimizi(mutluyum kelimesinin spn16 ile şifrelenmiş binary değeri) parametre olarak geçirdik, ve EncryptionDecryption sınıfının metodunu çağırdık
+            Assert.AreEqual("0110110101110101011101000110110001110101011110010111010101101101", sonuc); //ilk parametre(mutluyum kelimesinin binary değeri) 2. parametre ile aynı ise şifreleme methodumuz doğru çalışıyordur ve test başarılıdır
+        }
+
+        private string metinCoz(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test] //bu ifade bize bu metodun test metodu olduğunu ifade etmektedir
+        public void encrypt() //SHA_256_Encrypting test metodumuz
+        {
+            string sonuc = eD.encrypt("izmir"); //SHA_256_Encrypting methoduna değerimizi parametre olarak geçirdik, ve EncryptionDecryption sınıfının metodunu çağırdık
+            Assert.AreEqual("f3698e349001815961ee800a678db2d55d8f3b01e1ee7a402451c5b189cb3fc9", sonuc); //ilk parametre(mutluyum kelimesinin sha256 ile şifrelenmiş hali) 2. parametre ile aynı ise şifreleme methodumuz doğru çalışıyordur ve test başarılıdır
+        }
+
+        private string encrypt(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
